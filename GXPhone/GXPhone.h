d6c126994a40cc76b41bb9eaf8416dff27aa45f1
sunguangxin <sunguangxin@bilibili.com>
2017-08-10 17:06:09 +0800

    1.创建GXPhone工程.

---
+++ GXPhone/GXPhone.h
@@ -0,0 +1,20 @@
+//
+//  GXPhone.h
+//  GXPhone
+//
+//  Created by sunguangxin on 2017/8/10.
+//  Copyright © 2017年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GXPhone.
+FOUNDATION_EXPORT double GXPhoneVersionNumber;
+
+//! Project version string for GXPhone.
+FOUNDATION_EXPORT const unsigned char GXPhoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GXPhone/PublicHeader.h>
+
+#import <GXPhone/GXPhoneAppDelegate.h>
+
---
+++ GXPhone/GXPhoneAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GXPhoneAppDelegate.h
+//  GXPhone
+//
+//  Created by sunguangxin on 2017/8/10.
+//  Copyright © 2017年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GXPhoneAppDelegate : UIResponder
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
