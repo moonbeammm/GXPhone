5dfa6af7738619bfddb9a45a34683abb9463656e
sunguangxin <sunguangxin@bilibili.com>
2017-08-22 19:31:53 +0800

    1.给GXPhone工程建一个target壳

---
+++ GXShell/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GXShell
+//
+//  Created by sunguangxin on 2017/8/22.
+//  Copyright © 2017年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GXPhoneAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GXPhoneAppDelegate class]));
+    }
+}
